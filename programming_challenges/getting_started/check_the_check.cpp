c7f68c8bbe73913eb76079274a6cc55250d82790
HUMANIAM <eng.i.saad94@gmail.com>
2022-03-19 15:57:26 +0200

    add the solution of the trip problem

--- programming_challenges/getting_started/check_the_check.cpp
+++ programming_challenges/getting_started/check_the_check.cpp
@@ -277,60 +277,8 @@ int check_the_check()
     return -1;
 }
 
-int main() {
-    std::string cmd = "    latgen-faster-mapped"
-                      " --min-active=200 --max-active=7000 --beam=14     --lattice-beam=8 --acoustic-scale=1.0 --allow-partial=true --word-symbol-table=latgen-faster-mapped/input/test_4/words.txt"
-                      "     latgen-faster-mapped/input/test_4/in.mdl latgen-faster-mapped/input/test_4/in.fst ark:latgen-faster-mapped/input/test_4/logits.ark ark:latgen-faster-mapped/output/test_4/lat.ark";
-
-    std::regex options_regex (" ^--.*=.* ");
-
-     std::string lines[] = {"Roses are #ff0000",
-                           "violets are #0000ff",
-                           "all of my base are belong to you"};
- 
-    std::regex color_regex("#([a-f0-9]{2})"
-                            "([a-f0-9]{2})"
-                            "([a-f0-9]{2})");
- 
-    // simple match
-    for (const auto &line : lines) {
-        std::cout << line << ": " << std::boolalpha
-                  << std::regex_search(line, color_regex) << '\n';
-    }   
-    std::cout << '\n';
- 
-    // show contents of marked subexpressions within each match
-    std::smatch color_match;
-    for (const auto& line : lines) {
-        if(std::regex_search(line, color_match, color_regex)) {
-            std::cout << "matches for '" << line << "'\n";
-            std::cout << "Prefix: '" << color_match.prefix() << "'\n";
-            for (size_t i = 0; i < color_match.size(); ++i) 
-                std::cout << i << ": " << color_match[i] << '\n';
-            std::cout << "Suffix: '" << color_match.suffix() << "\'\n\n";
-        }
-    }
- 
-    // repeated search (see also std::regex_iterator)
-    std::string log(R"(
-        Speed:	366
-        Mass:	35
-        Speed:	378
-        Mass:	32
-        Speed:	400
-	Mass:	30)");
-    std::regex r(R"(Speed:\t\d*)");
-    std::smatch sm;
-    while(regex_search(log, sm, r))
-    {
-        std::cout << sm.str() << '\n';
-        log = sm.suffix();
-    }
- 
-    // C-style string demo
-    std::cmatch cm;
-    if(std::regex_search("this is a test", cm, std::regex("test"))) 
-        std::cout << "\nFound " << cm[0] << " at position " << cm.prefix().length();
+int main(int argc, const char** argv)
+{
     int tc = 1;
     while (true)
     {
--- programming_challenges/getting_started/the_trip.cpp
+++ programming_challenges/getting_started/the_trip.cpp
@@ -1,9 +1,62 @@
-#include <string>
 #include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <iomanip> 
 
+int costs[1000];
+int n;
+
+int to_cents(const std::string& str)
+{
+    size_t pos = str.find('.');
+    int dollars = std::stoi(str.substr(0, pos));
+    int cents = std::stoi(str.substr(pos+1));
+    return dollars*100 + cents;
+}
+
+int read_total_cents(int n)
+{
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        std::string s;
+        std::cin >> s;
+
+        costs[i] = to_cents(s);
+        sum += costs[i];
+    }
+
+    return sum;
+}
 
 int main() 
 {
+    int n;
 
+    while(true)
+    {
+        std::cin >> n;
+        if(n == 0) break;
+
+        double avg_cents = double(read_total_cents(n))/n;
+        double taken = 0, given = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            double diff = costs[i] - avg_cents;
+            if (diff < 0)
+            {
+                taken += -((int) diff) / 100.0;
+            } 
+            else
+            {
+                given += ((int) diff) / 100.0;
+            }
+        }
+
+        printf("$%.2f\n", taken > given ? taken : given);
+    }           
+      
     return 0;
 }
